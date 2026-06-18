50aa55e405d35e0d60edd42682a94680b2ed8875
mtcicer026 <tommywtullius@gmail.com>
2026-06-18 04:16:20 -0400

    Centralize best effort worker warmup

--- fiberhmm/inference/worker_warmup.py
+++ fiberhmm/inference/worker_warmup.py
@@ -7,6 +7,13 @@ import os
 import numpy as np
 
 
+def _run_best_effort_warmup(step) -> None:
+    try:
+        step()
+    except Exception:
+        pass
+
+
 def disable_numba_cache_locking() -> None:
     os.environ['NUMBA_CACHE_DIR'] = ''
 
@@ -16,10 +23,7 @@ def warm_up_model_predict(model) -> None:
 
     if HAS_NUMBA:
         dummy_obs = np.array([0, 1, 2, 3], dtype=np.int32)
-        try:
-            _ = model.predict(dummy_obs)
-        except Exception:
-            pass
+        _run_best_effort_warmup(lambda: model.predict(dummy_obs))
 
 
 def _posterior_warmup_obs(length: int) -> np.ndarray:
@@ -37,11 +41,12 @@ def warm_up_model_posteriors(model, length: int = 100) -> None:
         return
 
     dummy_obs = _posterior_warmup_obs(length)
-    try:
-        _ = model.predict(dummy_obs)
-        _ = model.predict_proba(dummy_obs)
-    except Exception:
-        pass
+
+    def _warm_up_predict_and_proba():
+        model.predict(dummy_obs)
+        model.predict_proba(dummy_obs)
+
+    _run_best_effort_warmup(_warm_up_predict_and_proba)
 
 
 def warm_up_tf_recaller(llr_hit, llr_miss) -> None:
@@ -50,10 +55,9 @@ def warm_up_tf_recaller(llr_hit, llr_miss) -> None:
     if HAS_NUMBA:
         from fiberhmm.inference.tf_recaller import call_tfs_in_interval
 
-        try:
-            _ = call_tfs_in_interval(
+        _run_best_effort_warmup(
+            lambda: call_tfs_in_interval(
                 _tf_warmup_obs(), 0, 16,
                 llr_hit, llr_miss, min_llr=4.0, min_opps=3,
             )
-        except Exception:
-            pass
+        )
